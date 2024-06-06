70b19cd908cf1276beaa1be48eee98f87418e5d3
Belchik Denis <denis1989g@mail.ru>
2024-06-06 16:43:51 +0400

    init

---
+++ src/main/java/com/example/psychology_back/PsychologyBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.psychology_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychologyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/psychology_back/PsychologyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psychology_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
