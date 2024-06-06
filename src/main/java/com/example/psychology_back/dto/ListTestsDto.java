d49e335a5f9ecb4ad26e89ef533cf188c145befb
Belchik Denis <denis1989g@mail.ru>
2024-06-06 18:34:19 +0400

    #1 Первоначальные настройки. Один эндпоинт getTests

---
+++ src/main/java/com/example/psychology_back/dto/ListTestsDto.java
@@ -0,0 +1,12 @@
+package com.example.psychology_back.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ListTestsDto {
+
+    private List<TestDto> tests;
+
+}
---
+++ src/main/java/com/example/psychology_back/mapper/TestMapper.java
@@ -0,0 +1,29 @@
+package com.example.psychology_back.mapper;
+
+import com.example.psychology_back.dto.ListTestsDto;
+import com.example.psychology_back.dto.TestDto;
+import com.example.psychology_back.entity.Test;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface TestMapper {
+    TestMapper INSTANCE = Mappers.getMapper(TestMapper.class);
+
+    @Mapping(target = "title", source = "title")
+    @Mapping(target = "description", source = "description")
+    TestDto testToTestDto(Test entity);
+
+
+    List<TestDto> testsToTestDtos(List<Test> entities);
+
+    default ListTestsDto testDtosToListTestsDto(List<Test> entities){
+        ListTestsDto listTestsDto = new ListTestsDto();
+        listTestsDto.setTests(testsToTestDtos(entities));
+        return listTestsDto;
+    }
+
+}
